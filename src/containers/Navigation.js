b0b7650bb0e1de728c960cdcd283425dd3d9e686
Sean Stephenson <sms@pixar.com>
2017-02-03 11:11:50 -0800

    add react bootstrap 4 classes

---
+++ src/containers/Navigation.js
@@ -0,0 +1,59 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { fetchNavIfNeeded } from '../actions';
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(fetchNavIfNeeded());
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  render() {
+    const { links } = this.props;
+    return (
+      <div>
+        <Navbar color="faded" light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              {links.items.map((l, i) =>
+                <NavItem key={i}>
+                  <NavLink href="/">{l.title}</NavLink>
+                </NavItem>
+              )}
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+Navigation.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  const navLinks = state.navLinks.data || { items: [] };
+  return {
+    links: navLinks,
+  };
+}
+
+export default connect(mapStateToProps)(Navigation);
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../configureStore';
-import Nav from './Nav';
+import Navigation from './Navigation';
 import Todo from './Todo';
 
 const store = configureStore();
@@ -15,8 +15,10 @@ export default class Root extends Component {
     return (
       <Provider store={store}>
         <div>
-          <Nav />
-          <Todo />
+          <Navigation />
+          <div className="container">
+            <Todo />
+          </div>
         </div>
       </Provider>
     );
